c1edf07f0220499dd37d832c9e9fc0c5c03c390b
cjw7250 <cjw7250@twotone.co.kr>
2021-04-20 16:59:32 +0900

    add demo data

---
+++ lib/data/Login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Login {
+  final String email;
+  final String password;
+
+  Login({@required this.email, @required this.password});
+}
+
+List<Login> loginData = [
+  Login(email: 'cjw7250@twotone.co.kr', password: 'asfg1245!'),
+];
