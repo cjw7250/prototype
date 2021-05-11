7b4e10ad09fdf41ff67acf8cb461c93ad2f0cdda
cjw7250 <cjw7250@twotone.co.kr>
2021-05-11 18:03:02 +0900

    update add_todo_screen

--- lib/screens/add_todo/component/body.dart
+++ lib/screens/add_todo/component/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prototype_app/constants.dart';
+import 'package:prototype_app/size_config.dart';
 
 class Body extends StatelessWidget {
   const Body({Key key}) : super(key: key);
@@ -9,21 +10,89 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         Padding(
-          padding: EdgeInsets.all(kDefaultPadding),
-          child: TextFormField(
-            decoration: InputDecoration(
-              labelText: '품명',
-              border: OutlineInputBorder(),
+          padding: EdgeInsets.fromLTRB(
+              kDefaultPadding, kDefaultPadding, kDefaultPadding, 0),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 2,
+                color: kPrimaryColor,
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 5,
+                    ),
+                    child: Text(
+                      '품명',
+                      style: TextStyle(
+                        fontSize: getProportionateScreenWidth(25),
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(0),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(
+                        color: kPrimaryColor,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      '품목 찾기',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: getProportionateScreenWidth(25),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
         Padding(
-          padding: EdgeInsets.all(kDefaultPadding),
-          child: TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: '수량',
-              border: OutlineInputBorder(),
+          padding: EdgeInsets.fromLTRB(
+              kDefaultPadding, kDefaultPadding, kDefaultPadding, 0),
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 5,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 2,
+                color: kPrimaryColor,
+              ),
+            ),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: '수량',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: getProportionateScreenWidth(25),
+                    ),
+                    focusedBorder: InputBorder.none,
+                    border: InputBorder.none,
+                  ),
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(25),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
